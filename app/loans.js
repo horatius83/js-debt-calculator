b77ce506085238f3cba67961f93e74f3432046ab
M Monster <horatius83@gmail.com>
2020-04-05 21:03:39 -0500

    Now rendering loans

--- app/loans.js
+++ app/loans.js
@@ -1,4 +1,7 @@
 Vue.component('loans', {
+    props: {
+        loans: Array
+    },
     template: `
         <table>
             <thead>
@@ -6,7 +9,15 @@ Vue.component('loans', {
                 <th>Principal</th>
                 <th>Interest</th>
                 <th>Minimum</th>
+                <th></th>
             </thead>
+            <tr v-for="loan in loans">
+                <td>{{ loan.name }}</td>
+                <td>{{ loan.principal }}</td>
+                <td>{{ loan.interest }}</td>
+                <td>{{ loan.minimum }}</td>
+                <td><input type="button" value="Delete"></input></td>
+            </tr>
         </table>
     `
 });
