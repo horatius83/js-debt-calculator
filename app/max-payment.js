fdeee60afd7df39cb7b3e71019a50bd9c4b5925b
M Monster <horatius83@gmail.com>
2020-04-10 23:29:48 -0500

    Working on validation for max-payment control

--- app/max-payment.js
+++ app/max-payment.js
@@ -1,9 +1,29 @@
 Vue.component('max-payment', {
+    data: function() {
+        return {
+            totalMonthlyPayment: 0
+        }
+    },
+    props: {
+        loans: Array
+    },
+    computed: {
+        minimumPayment: function() {
+            const date = new Date();
+            const payments = this.loans
+                .map(ln => new Payment(ln, ln.principal, 0, date))
+                .map(p => p.getMinimumMonthlyPayment(date));
+            return payments.reduce((acc, x) => acc + x.amountPaid, 0);
+        }
+    },
     template: `
         <div class="row">
             <div class="card large">
-                <label for="max-payment">Maximum Payment</label>
-                <input name="max-payment" id="max-payment" type="number"></input>
+                <label for="max-payment">Total monthly Payment</label>
+                <input name="max-payment" id="max-payment" type="number" v-bind:value="totalMonthlyPayment"></input>
+                <div v-if="minimumPayment > totalMonthlyPayment" class="card error">
+                    Total monthly payment must exceed minimum required payment.
+                </div>
             </div>
             <div class="card large">
                 <label for="loan-payment-strategy">Loan Payment Strategy</label>
