dd4232c0c8a7a4964b7bb5711979b82eb59956b9
M Monster <horatius83@gmail.com>
2020-04-18 13:28:33 -0500

    Added router, moved code in index into its own component

---
+++ app/loans-summary.vue.js
@@ -0,0 +1,16 @@
+Vue.component('loans-summary', {
+    props: {
+        'loans': Array
+    },
+    template: `
+        <div id="loans-summary">
+            <h1>Debt Calculator</h1>
+            <div id="loans-summary-body">
+                <max-payment v-bind:loans="loans"></max-payment>
+                <loans v-bind:loans="loans"></loans>
+                <new-loan></new-loan>
+                <loan-graph></loan-graph>
+            </div>
+        </div>
+    `
+});
