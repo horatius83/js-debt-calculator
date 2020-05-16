a861aa7181986a46556ced8f96c2f9777fbe04cb
M. Monster <horatius83@gmail.com>
2020-05-15 23:22:36 -0500

    Added plan page

--- app/components/main.vue.js
+++ app/components/main.vue.js
@@ -1,10 +1,11 @@
 import { loansSummary } from './loans-summary.vue.js'
 import { GraphPage } from './graph-page.vue.js';
+import { PlanPage } from './plan-page.vue.js';
 
 const routes = [
     { path: '/', component: loansSummary },
     { path: '/graph', component: GraphPage },
-    { path: '/plan', component: loansSummary },
+    { path: '/plan', component: PlanPage },
     { path: '/import', component: loansSummary },
     { path: '/export', component: loansSummary },
 ];
---
+++ app/components/plan-page.vue.js
@@ -0,0 +1,7 @@
+export var PlanPage = Vue.component('plan-page', {
+    template: `
+    <div>
+        <h1>Plan Page</h1>
+    </div>
+    `
+});
