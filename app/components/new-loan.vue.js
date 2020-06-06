db913f66cbcd720f052b6ee8482939a1f73b7eb5
Max Monster <horatius83@gmail.com>
2020-06-06 18:26:28 -0500

    Quality of life improvements, focus and tab correctness

--- app/components/new-loan.vue.js
+++ app/components/new-loan.vue.js
@@ -12,6 +12,13 @@ var NewLoan = Vue.component('new-loan', {
         };
     },
     methods: {
+        addNewLoan: function() {
+            this.validateForm();
+            if(this.errors.length === 0) {
+                this.$emit('add-new-loan', new Loan(this.name, this.principal, this.interest, this.minimum));
+                this.clear();
+            }
+        },
         clear: function() {
             this.shouldDisplayForm = false;
             this.name = '';
@@ -19,13 +26,16 @@ var NewLoan = Vue.component('new-loan', {
             this.interest = 0;
             this.minimum = 0;
             this.errors = [];
+            this.$nextTick().then(() => {
+                document.getElementById("addNewLoanButton")?.focus();
+            });
         },
-        addNewLoan: function() {
-            this.validateForm();
-            if(this.errors.length === 0) {
-                this.$emit('add-new-loan', new Loan(this.name, this.principal, this.interest, this.minimum));
-                this.clear();
-            }
+        displayForm: function() {
+            this.shouldDisplayForm = true;
+            this.$nextTick().then(() => {
+                document.getElementById('new-loan-name')?.focus();
+                this.errors = [];
+            });
         },
         validateForm: function() {
             this.errors = [];
@@ -39,27 +49,63 @@ var NewLoan = Vue.component('new-loan', {
                 this.errors.push('Loan needs a minimum payment');
             }
             return this.errors.length === 0;
+        },
+    },
+    computed: {
+        shouldDisplayErrors: function() {
+            return (this.errors.length !== 0);
         }
+    },
+    mounted: function() {
+        document.getElementById("addNewLoanButton")?.focus();
     },
     template: `
         <div class="card fluid">
             <div v-if="!shouldDisplayForm">
-                <button v-on:click="shouldDisplayForm = true">Add New Loan</button>
+                <button v-on:click="displayForm" id="addNewLoanButton">Add New Loan</button>
             </div>
             <div v-if="shouldDisplayForm">
                 <label for="new-loan-name">Name</label>
-                <input name="new-loan-name" id="new-loan-name" v-model="name" required="required"></input>
+                <input 
+                    name="new-loan-name" 
+                    id="new-loan-name" 
+                    v-model="name" 
+                    required="required" 
+                ></input>
                 <label for="new-loan-principal">Principal</label>
-                <input name="new-loan-principal" type="number" inputmode="decimal" v-model.number="principal" step="0.01" min="0"></input>
+                <input 
+                    name="new-loan-principal" 
+                    type="number" 
+                    inputmode="decimal" 
+                    v-model.number="principal" 
+                    step="0.01" 
+                    min="0"
+                ></input>
                 <label for="new-loan-interest">Interest</label>
-                <input name="new-loan-interest" id="new-loan-interest" type="number" inputmode="decimal" v-model.number="interest" step="0.01" min="0"></input>
+                <input 
+                    name="new-loan-interest" 
+                    id="new-loan-interest" 
+                    type="number" 
+                    inputmode="decimal" 
+                    v-model.number="interest" 
+                    step="0.01" 
+                    min="0"
+                ></input>
                 <label for="new-loan-minimum">Minimum</label>
-                <input name="new-loan-minimum" id="new-loan-minimum" type="number" inputmode="decimal" v-model.number="minimum" step="0.01" min="0"></input>
+                <input 
+                    name="new-loan-minimum" 
+                    id="new-loan-minimum" 
+                    type="number" 
+                    inputmode="decimal" 
+                    v-model.number="minimum" 
+                    step="0.01" 
+                    min="0"
+                ></input>
                 <div class="row">
                     <button class="primary" v-on:click="addNewLoan()">Create New Loan</button>
                     <button class="secondary" v-on:click="clear()">Cancel</button>
                 </div>
-                <div v-if="errors.length > 0">
+                <div v-if="shouldDisplayErrors">
                     <ul>
                         <li v-for="error in errors" class="error-message">{{ error }}</li>
                     </ul>
