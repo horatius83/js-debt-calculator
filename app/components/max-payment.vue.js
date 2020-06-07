3f40b1ba257321c4f18de98280b788c4134b082d
Max Monster <horatius83@gmail.com>
2020-06-06 19:20:10 -0500

    Added better error messaging, and information on the  minimum payment

--- app/components/max-payment.vue.js
+++ app/components/max-payment.vue.js
@@ -32,17 +32,18 @@ var MaxPayment = Vue.component('max-payment', {
     },
     template: `
         <div class="card large col-sm">
-            <label for="max-payment">Total monthly Payment</label>
+            <label for="max-payment">Total monthly Payment (Minimum: {{ minimumPayment | currency }})</label>
             <input 
                 name="max-payment" 
                 id="max-payment" 
                 type="number" 
                 v-model.number="totalMonthlyPayment"
                 step="0.01"
+                minimum="{{ minimumPayment }}"
                 v-on:change="updateTotalMonthlyPayment($event.target.value)"
             ></input>
             <div v-if="minimumPayment > totalMonthlyPayment">
-                <span class="error-message">Total monthly payment must exceed minimum required payment.</span>
+                <span class="error-message">If your payment is less than the minimum then the loan will never be repaid (it will accrue interest in excess of your payment).</span>
             </div>
         </div>
     `
