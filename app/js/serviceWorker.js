a2cb70fdae0c20c477511422ccc2ed59b5672f41
M Monster <horatius83@gmail.com>
2020-07-02 23:44:00 -0500

    Still working on getting PWA working

--- app/js/serviceWorker.js
+++ app/js/serviceWorker.js
@@ -1,38 +1,45 @@
 const cacheName = 'loans-pwa';
 const filestoCache = [
     // JS Files
-    '/services/loan-service.js',
-    '/js/vue-router.js',
-    '/js/Chart.js',
-    '/js/serviceWorker.js',
-    '/js/accounting.min.js',
-    '/js/vue.js',
-    '/components/new-loan.vue.js',
-    '/components/loans-summary.vue.js',
-    '/components/loan-strategy.vue.js',
-    '/components/main.vue.js',
-    '/components/plan-page.vue.js',
-    '/components/loan-graph.vue.js',
-    '/components/payment-summary.vue.js',
-    '/components/payment-plan-component.vue.js',
-    '/components/filters.vue.js',
-    '/components/max-payment.vue.js',
-    '/components/loans.vue.js',
-    '/components/graph-page.vue.js',
-    '/models/loan/paymentPlan.js',
-    '/models/loan/loan.js',
-    '/models/loan/loanPaymentPlan.js',
-    '/models/loan/payment.js',
-    '/models/loan/paymentStrategy.js',
-    '/models/util/interest.js',
+    '../services/loan-service.js',
+    'vue-router.js',
+    'Chart.js',
+    'serviceWorker.js',
+    'accounting.min.js',
+    'vue.js',
+    '../components/new-loan.vue.js',
+    '../components/loans-summary.vue.js',
+    '../components/loan-strategy.vue.js',
+    '../components/main.vue.js',
+    '../components/plan-page.vue.js',
+    '../components/loan-graph.vue.js',
+    '../components/payment-summary.vue.js',
+    '../components/payment-plan-component.vue.js',
+    '../components/filters.vue.js',
+    '../components/max-payment.vue.js',
+    '../components/loans.vue.js',
+    '../components/graph-page.vue.js',
+    '../models/loan/paymentPlan.js',
+    '../models/loan/loan.js',
+    '../models/loan/loanPaymentPlan.js',
+    '../models/loan/payment.js',
+    '../models/loan/paymentStrategy.js',
+    '../models/util/interest.js',
 
     // CSS Files
-    '/css/loans.css',
-    '/css/mini-dark.min.css',
+    '../css/loans.css',
+    '../css/mini-dark.min.css',
 
     // HTML Files
-    '/',
-    '/index.html'
+    '../index.html',
+
+    // Images
+    "../images/icons/icon-128x128.png",
+    "../images/icons/icon-144x144.png",
+    "../images/icons/icon-152x152.png",
+    "../images/icons/icon-192x192.png",
+    "../images/icons/icon-256x256.png",
+    "../images/icons/icon-512x512.png"
 ];
 
 // Start the service worker to cache all the content
