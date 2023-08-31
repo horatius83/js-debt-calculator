ab4594507030e81acb0379d039dbab92952cc62d
M Monster <horatius83@gmail.com>
2023-08-30 21:47:38 -0500

    Removed loan.mjs since it was not contributing much

--- app/modules/paymentPlan.mjs
+++ app/modules/paymentPlan.mjs
@@ -1,5 +1,20 @@
-import { Loan } from './loan.mjs';
 import { getMinimumMonthlyPaymentWithinPeriod } from './interest.mjs';
+
+export class Loan {
+    /**
+     * Class represnting an amount of money owed
+     * @param {string} name - the name of the loan
+     * @param {number} principal - the principal of the loan
+     * @param {number} interest - the interest (APR)
+     * @param {number} minimum - the minimum payment
+     */
+    constructor(name, principal, interest, minimum) {
+        this.name = name;
+        this.principal = principal;
+        this.interest = interest;
+        this.minimum = minimum;
+    }
+}
 
 export class Payment {
     /**
