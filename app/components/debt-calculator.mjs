93b4a1e7d183f910e897462dfe30d0a16ff7960d
M Monster <horatius83@gmail.com>
2023-09-17 15:33:41 -0500

    Add new loan functionality

--- app/components/debt-calculator.mjs
+++ app/components/debt-calculator.mjs
@@ -1,6 +1,15 @@
 import { Loan } from "../modules/paymentPlan.mjs";
 import { LoanService } from "../services/loan-service.mjs";
 
+class NewLoanState {
+    constructor() {
+        this.name = "";
+        this.principal = "";
+        this.interest = "";
+        this.minimum = "";
+    }
+}
+
 class DebtCalculatorState {
     constructor() {
         /** @type {Array<Loan>} */
@@ -22,6 +31,7 @@ class DebtCalculatorState {
             new Loan("Military Star", 799.68, 10.49, 45),
             new Loan("Sears", 3797.66, 25.44, 122)
         ];
+        this.newLoan = new NewLoanState();
     }
 }
 
@@ -32,12 +42,29 @@ class DebtCalculatorState {
  * @returns {Loan[]} - Either the loans array without loan or the original array if loan was not found
  */
 function deleteLoan(loanName, loans) {
-    debugger;
     const index = loans.findIndex(x => x.name === loanName);
+
     if (index >= 0) {
-        return loans.splice(index, 1);
+        loans.splice(index, 2);
     }
     return loans;
+}
+
+/**
+ * Attempt to create a loan from a name, principal, interest, and minimum value
+ * @param {string} name - the name of the loan
+ * @param {string} principal - the amount of money owed on the loan
+ * @param {string} interest - the interest on the loan
+ * @param {string} minimum - the minimum monthly payment on the loan
+ * @returns {Loan | undefined} - Either a Loan if the values are valid or undefined
+ */
+function getLoan(name, principal, interest, minimum) {
+    const p = Number(principal);
+    const i = Number(interest);
+    const m = Number(minimum);
+    if (p && i && m && name) {
+        return new Loan(name, p, i, m);
+    }
 }
 
 export const DebtCalculator = {
@@ -51,7 +78,9 @@ export const DebtCalculator = {
          * @returns {string} - value formatted as a currency ($3.14)
          */
         asCurrency(value) {
-            return `$${value.toFixed(2)}`;
+            const x = Math.round(value * 100)
+            const y = x / 100.0;
+            return `$${y.toFixed(2)}`;
         },
         /**
          * Format a value as a percentage
@@ -59,33 +88,76 @@ export const DebtCalculator = {
          * @returns {string}
          */
         asPercentage(value) {
-            return `${value}%`;
+            return `${value.toFixed(2)}%`;
         },
         /**
          * Delete a Loan
+         * @param { string } loanName - name of th eloan
          */
         removeLoan(loanName) {
-            debugger;
             this.loans = deleteLoan(loanName, this.loans);
+        },
+
+        addLoan() {
+            const loan = getLoan(
+                this.newLoan.name, 
+                this.newLoan.principal, 
+                this.newLoan.interest, 
+                this.newLoan.minimum);
+            if (loan) {
+                this.loans.push(loan);
+            }
         }
     },
     template: 
 /* html */`
-<table id="loans-table">
-    <thead>
-        <th class="loans-table-name-column">Name</th>
-        <th class="loans-table-principal-column">Principal</th>
-        <th class="loans-table-interest-column">Interest</th>
-        <th class="loans-table-minimum-column">Minimum</th>
-        <th class="loans-table-delete-column"></th>
-    </thead>
-    <tr v-for="loan in loans">
-        <td data-label="Name">{{ loan.name }}</td>
-        <td data-label="Principal">{{ asCurrency(loan.principal) }}</td>
-        <td data-label="Interest">{{ asPercentage(loan.interest) }}</td>
-        <td data-label="Minimum">{{ asCurrency(loan.minimum) }}</td>
-        <td><button class="secondary" v-on:click="removeLoan(loan.name)">Delete</button></td>
-    </tr>
-</table>
+<div>
+    <table id="loans-table">
+        <thead>
+            <th class="loans-table-name-column">Name</th>
+            <th class="loans-table-principal-column">Principal</th>
+            <th class="loans-table-interest-column">Interest</th>
+            <th class="loans-table-minimum-column">Minimum</th>
+            <th class="loans-table-delete-column"></th>
+        </thead>
+        <tr v-for="loan in loans">
+            <td data-label="Name">{{ loan.name }}</td>
+            <td data-label="Principal">{{ asCurrency(loan.principal) }}</td>
+            <td data-label="Interest">{{ asPercentage(loan.interest) }}</td>
+            <td data-label="Minimum">{{ asCurrency(loan.minimum) }}</td>
+            <td><button class="secondary" v-on:click="removeLoan(loan.name)">Delete</button></td>
+        </tr>
+    </table>
+    <div id="summary" class="card fluid">
+        <label for="new-loan-name">Name</label>
+        <input 
+            name="new-loan-name" 
+            id="new-loan-name" 
+            v-model="newLoan.name" 
+            required="required" 
+        >
+        <label for="new-loan-principal">Principal</label>
+        <input 
+            name="new-loan-principal" 
+            v-model="newLoan.principal" 
+        >
+        <label for="new-loan-interest">Interest</label>
+        <input 
+            name="new-loan-interest" 
+            id="new-loan-interest" 
+            v-model="newLoan.interest" 
+        >
+        <label for="new-loan-minimum">Minimum</label>
+        <input 
+            name="new-loan-minimum" 
+            id="new-loan-minimum" 
+            v-model="newLoan.minimum" 
+        >
+        <div class="row">
+            <button class="primary" v-on:click="addLoan">Create New Loan</button>
+            <button class="secondary" v-on:click="clear()">Cancel</button>
+        </div>
+    </div>
+</div>
 `
 };
